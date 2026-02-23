d9d3c08a87662cf10b83201ae4e8b27d01872cdd
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2026-02-23 21:21:51 +0100

    Fixates the MAC address for virt-install and virtualbox
    Fixes broken networking in virtualbox

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ wget https://cloud.debian.org/images/cloud/trixie/latest/debian-13-generic-amd64
 sudo cp debian-13-generic-amd64.qcow2 /var/lib/libvirt/images/
 
 # create VM and start provisioning
-virt-install --name tchecker --memory 8192 --vcpus 4 --disk=size=10,backing_store=/var/lib/libvirt/images/debian-13-generic-amd64.qcow2 --cloud-init user-data=./cloud-init.yaml,disable=on --network bridge=virbr0 --osinfo=debian13
+virt-install --name tchecker --memory 8192 --vcpus 4 --disk=size=10,backing_store=/var/lib/libvirt/images/debian-13-generic-amd64.qcow2 --cloud-init user-data=./cloud-init.yaml,disable=on --network bridge=virbr0,mac=52:54:00:80:26:95 --osinfo=debian13
 # ^the VM terminates itself after provisioning
 
 # create OVA template
